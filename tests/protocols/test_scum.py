e34c08148e6f7d15a81b4127e56cccd120b19919
Battlefield Duck <hkdsethomas@gmail.com>
2023-06-24 03:35:53 +0800

    Update tests

--- tests/protocols/test_scum.py
+++ tests/protocols/test_scum.py
@@ -1,17 +1,22 @@
 import os
 
 import pytest
-from opengsq.protocols.satisfactory import Satisfactory
+from opengsq.protocols.scum import Scum
 
 from .result_handler import ResultHandler
 
 handler = ResultHandler(os.path.basename(__file__)[:-3])
 # handler.enable_save = True
 
-# Satisfactory
-test = Satisfactory(host='delta3.ptse.host', port=15777)
+# Scum
+test = Scum(host='15.235.181.19', port=7042)
 
 @pytest.mark.asyncio
 async def test_get_status():
     result = await test.get_status()
     await handler.save_result('test_get_status', result)
+
+@pytest.mark.asyncio
+async def test_query_master_servers():
+    result = await test.query_master_servers()
+    await handler.save_result('test_query_master_servers', result)
