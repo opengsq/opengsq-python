6ce16142c948d08fa6eeedd03fb95baa9d5eed97
Steven Wesner <stwesner@live.com>
2025-01-18 18:16:52 -0500

    Palworld Test
    Added test for Palworld

---
+++ tests/protocols/test_palworld.py
@@ -0,0 +1,21 @@
+import pytest
+from opengsq.protocols.palworld import Palworld
+
+from ..result_handler import ResultHandler
+
+handler = ResultHandler(__file__)
+# handler.enable_save = True
+
+# Palworld
+test = Palworld(
+    host="79.136.0.124",
+    port=8212,
+    app_username="admin",
+    app_password="",
+)
+
+
+@pytest.mark.asyncio
+async def test_get_status():
+    result = await test.get_status()
+    await handler.save_result("test_get_status", result)
