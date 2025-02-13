34d37fbb26a05149563d55bf1c23eddfa4de2b04
Hornochs <chaos_bloated856@memehorst.de>
2025-02-13 16:03:55 +0100

    Revovering File

--- opengsq/protocols/toxikk.py
+++ opengsq/protocols/toxikk.py
@@ -61,7 +61,7 @@ class Toxikk(UDK):
                 toxikk_properties['numbots'] = prop['data']
             elif prop_id == 1073741828:  # Mutators
                 base_response['mutators'] = self._parse_mutators(prop['data'])
-                #toxikk_properties['mutators'] = self._parse_mutators(prop['data'])
+                toxikk_properties['mutators'] = self._parse_mutators(prop['data'])
 
         for setting in base_response['raw']['localized_settings']:
             setting_id = setting['id']
