2d57a6984c91e912da48aaf565d98c3176b88158
Hornochs <chaos_bloated856@memehorst.de>
2025-02-13 15:54:57 +0100

    Updating status.py

--- opengsq/responses/toxikk/status.py
+++ opengsq/responses/toxikk/status.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Optional
 from opengsq.responses.udk.status import Status as UDKStatus
 
 @dataclass
 class Status(UDKStatus):
     """Toxikk-specific status response"""
-    mutators: List[str] = None
+    mutators: Optional[List[str]] = field(default_factory=list)
