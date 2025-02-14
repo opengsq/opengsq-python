c11de5b1f27e23fe647274ce4a70dcb288b5beae
Hornochs <chaos_bloated856@memehorst.de>
2025-02-14 14:10:32 +0100

    Implementing first RenegadeX Query

---
+++ opengsq/responses/renegadex/status.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class Variables:
+    player_limit: int
+    vehicle_limit: int
+    mine_limit: int
+    time_limit: int
+    passworded: bool
+    steam_required: bool
+    team_mode: int
+    spawn_crates: bool
+    game_type: int
+    ranked: bool
+    
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> 'Variables':
+        return cls(
+            player_limit=data["Player Limit"],
+            vehicle_limit=data["Vehicle Limit"],
+            mine_limit=data["Mine Limit"],
+            time_limit=data["Time Limit"],
+            passworded=data["bPassworded"],
+            steam_required=data["bSteamRequired"],
+            team_mode=data["Team Mode"],
+            spawn_crates=data["bSpawnCrates"],
+            game_type=data["Game Type"],
+            ranked=data["bRanked"]
+        )
+
+@dataclass
+class Status:
+    name: str
+    current_map: str
+    port: int
+    players: int
+    game_version: str
+    variables: Variables
+    raw: dict[str, Any]
+    
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> 'Status':
+        return cls(
+            name=data["Name"],
+            current_map=data["Current Map"],
+            port=data["Port"],
+            players=data["Players"],
+            game_version=data["Game Version"],
+            variables=Variables.from_dict(data["Variables"]),
+            raw=data
+        )
